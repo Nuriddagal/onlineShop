fa03294c56d8e025472cf79dda0eda1bab6696a5
Nuriddagal <creakgam@gmail.com>
2025-06-24 20:45:34 +0300

    fix

--- src/pages/product-card.tsx
+++ src/pages/product-card.tsx
@@ -14,12 +14,12 @@ export function ProductCard({product, addTo, setShowModal}: CardState) {
                         className="products__image"
                         />
                     <button className="favorite-icon" onClick={() => setShowModal?setShowModal(true):''}>
-                        <img src="../assets/heart.png" alt="to favorite"/>
+                        <img src="assets/heart.png" alt="to favorite"/>
                     </button>
                     </div>
                     <p className='products__price'>${product.price}</p>
                     <p className='products__title'>{product.title}</p>
-                    <p className='products__rating'><img src="./src/assets/icons848.png" alt="star" className='products__star' />{product.rating}</p>
+                    <p className='products__rating'><img src="assets/icons848.png" alt="star" className='products__star' />{product.rating}</p>
                     <button className='basket-button' onClick={() => addTo(product)}>
                         <p>{product.shippingInformation.replace("Ships", '')}</p><BasketSvg/>
                     </button>
