a72c5aad350c3ecbc8b680d40fdd77b40e6de418
Nuriddagal <creakgam@gmail.com>
2025-06-24 21:24:02 +0300

    fixed again

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ import { BrowserRouter } from 'react-router'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename='/onlineShop/'>
+      <BrowserRouter basename='/'>
         <App />
       </BrowserRouter>
     </Provider>
