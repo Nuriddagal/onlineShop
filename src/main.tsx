42d3f4e99eeb6e226f94d969b8be442cbe092b56
Nuriddagal <creakgam@gmail.com>
2025-06-30 21:20:59 +0300

    fixed media

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import { Provider } from 'react-redux'
-import { store } from './redux/state.ts'
+import { store } from './redux/state'
  
 import './styles/index.css'
-import App from './App.tsx'
+import App from './App'
 import { HashRouter } from 'react-router'
 
 
